b9b702f2ad2b7c96b78ed461dda4ab1a4b2e6df7
Liam Huang <liamhuang0205@gmail.com>
2019-07-16 14:42:34 +0800

    Site updated: 2019-07-16 14:41:53

--- js/utils.js
+++ js/utils.js
@@ -1,20 +1,20 @@
 /* global NexT, CONFIG */
 
-NexT.utils = NexT.$u = {
+NexT.utils = {
 
   /**
    * Wrap images with fancybox support.
    */
   wrapImageWithFancyBox: function() {
     $('.content img')
-      .not(':hidden')
+      .not('#qr img')
       .each(function() {
         var $image = $(this);
         var imageTitle = $image.attr('title') || $image.attr('alt');
         var $imageWrapLink = $image.parent('a');
 
         if ($imageWrapLink.length < 1) {
-          var imageLink = $image.attr('data-original') || $image.attr('src');
+          var imageLink = $image.attr('data-src') || $image.attr('src');
           $imageWrapLink = $image.wrap('<a class="fancybox fancybox.image" href="' + imageLink + '" itemscope itemtype="http://schema.org/ImageObject" itemprop="url"></a>').parent('a');
           if ($image.is('.post-gallery img')) {
             $imageWrapLink.addClass('post-gallery-img');
@@ -46,10 +46,59 @@ NexT.utils = NexT.$u = {
   },
 
   lazyLoadPostsImages: function() {
-    $('#posts').find('img').lazyload({
-      //placeholder: '/images/loading.gif',
-      effect   : 'fadeIn',
-      threshold: 0
+    lozad('.content img').observe();
+  },
+
+  /**
+   * One-click copy code support.
+   */
+  registerCopyCode: function() {
+    $('.highlight').not('.gist .highlight').each(function(i, e) {
+      function initButton(button) {
+        if (CONFIG.copycode.style === 'mac') {
+          button.html('<i class="fa fa-clipboard"></i>');
+        } else {
+          button.text(CONFIG.translation.copy_button);
+        }
+      }
+      var $button = $('<div>').addClass('copy-btn');
+      $button.on('click', function() {
+        var code = $(this).parent().find('.code').find('.line').map(function(i, e) {
+          return $(e).text();
+        }).toArray().join('\n');
+        var ta = document.createElement('textarea');
+        var yPosition = window.pageYOffset || document.documentElement.scrollTop;
+        ta.style.top = yPosition + 'px'; // Prevent page scroll
+        ta.style.position = 'absolute';
+        ta.style.opacity = '0';
+        ta.readOnly = true;
+        ta.value = code;
+        document.body.appendChild(ta);
+        const selection = document.getSelection();
+        const selected = selection.rangeCount > 0 ? selection.getRangeAt(0) : false;
+        ta.select();
+        ta.setSelectionRange(0, code.length);
+        ta.readOnly = false;
+        var result = document.execCommand('copy');
+        if (CONFIG.copycode.show_result) {
+          $(this).text(result ? CONFIG.translation.copy_success : CONFIG.translation.copy_failure);
+        }
+        ta.blur(); // For iOS
+        $(this).blur();
+        if (selected) {
+          selection.removeAllRanges();
+          selection.addRange(selected);
+        }
+        document.body.removeChild(ta);
+      });
+      $button.on('mouseleave', function() {
+        var $b = $(this).closest('.copy-btn');
+        setTimeout(function() {
+          initButton($b);
+        }, 300);
+      });
+      initButton($button);
+      $(e).wrap($('<div>').addClass('highlight-wrap')).after($button);
     });
   },
 
@@ -125,7 +174,7 @@ NexT.utils = NexT.$u = {
     });
 
     $top.on('click', function() {
-      $.isFunction($('html').velocity) ? $('body').velocity('scroll') : $('html, body').animate({ scrollTop: 0 });
+      $('html, body').animate({ scrollTop: 0 });
     });
   },
 
@@ -279,7 +328,7 @@ NexT.utils = NexT.$u = {
   },
 
   getSidebarb2tHeight: function() {
-    var sidebarb2tHeight = (CONFIG.back2top && CONFIG.back2top_sidebar) ? $('.back-to-top').height() : 0;
+    var sidebarb2tHeight = (CONFIG.back2top.enable && CONFIG.back2top.sidebar) ? $('.back-to-top').height() : 0;
     return sidebarb2tHeight;
   },
 
@@ -294,47 +343,3 @@ NexT.utils = NexT.$u = {
     return sidebarSchemePadding;
   }
 };
-
-$(document).ready(function() {
-
-  function wrapTable() {
-    $('table').not('.gist table').wrap('<div class="table-container"></div>');
-  }
-
-  /**
-   * Init Sidebar & TOC inner dimensions on all pages and for all schemes.
-   * Need for Sidebar/TOC inner scrolling if content taller then viewport.
-   */
-  function updateSidebarHeight(height) {
-    height = height || 'auto';
-    $('.site-overview, .post-toc').css('max-height', height);
-  }
-
-  function initSidebarDimension() {
-    var updateSidebarHeightTimer;
-
-    $(window).on('resize', function() {
-      updateSidebarHeightTimer && clearTimeout(updateSidebarHeightTimer);
-
-      updateSidebarHeightTimer = setTimeout(function() {
-        var sidebarWrapperHeight = document.body.clientHeight - NexT.utils.getSidebarSchemePadding();
-
-        updateSidebarHeight(sidebarWrapperHeight);
-      }, 0);
-    });
-
-    // Initialize Sidebar & TOC Width.
-    var scrollbarWidth = NexT.utils.getScrollbarWidth();
-    if ($('.site-overview-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-      $('.site-overview').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
-    }
-    if ($('.post-toc-wrap').height() > (document.body.clientHeight - NexT.utils.getSidebarSchemePadding())) {
-      $('.post-toc').css('width', 'calc(100% + ' + scrollbarWidth + 'px)');
-    }
-
-    // Initialize Sidebar & TOC Height.
-    updateSidebarHeight(document.body.clientHeight - NexT.utils.getSidebarSchemePadding());
-  }
-  initSidebarDimension();
-  wrapTable();
-});
