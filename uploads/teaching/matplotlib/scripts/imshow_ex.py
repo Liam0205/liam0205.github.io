dacc8cb58528d45670dadbcd5656baa83912e6dc
Liam Huang <liamhuang0205@gmail.com>
2019-08-30 00:14:17 +0800

    Site updated: 2019-08-30 00:14:09

---
+++ uploads/teaching/matplotlib/scripts/imshow_ex.py
@@ -0,0 +1,23 @@
+# -----------------------------------------------------------------------------
+# Copyright (c) 2015, Nicolas P. Rougier. All Rights Reserved.
+# Distributed under the (new) BSD License. See LICENSE.txt for more info.
+# -----------------------------------------------------------------------------
+import numpy as np
+import matplotlib.pyplot as plt
+
+def f(x,y):
+    return (1-x/2+x**5+y**3)*np.exp(-x**2-y**2)
+
+n = 10
+x = np.linspace(-3,3,3.5*n)
+y = np.linspace(-3,3,3.0*n)
+X,Y = np.meshgrid(x,y)
+Z = f(X,Y)
+
+plt.axes([0.025,0.025,0.95,0.95])
+plt.imshow(Z,interpolation='nearest', cmap='bone', origin='lower')
+plt.colorbar(shrink=.92)
+
+plt.xticks([]), plt.yticks([])
+# savefig('../figures/imshow_ex.png', dpi=48)
+plt.show()
